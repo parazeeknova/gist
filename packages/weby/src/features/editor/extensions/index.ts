08a441ae568ed103466df6f71f812be5c915df74
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-05-10 23:22:25 +0530

    feat[EDITOR]: add TipTap extension registry with table, task-list, text-align, highlight, super/subscript, placeholder

---
+++ packages/weby/src/features/editor/extensions/index.ts
@@ -0,0 +1,60 @@
+// eslint-disable-next-line import/no-named-as-default
+import StarterKit from "@tiptap/starter-kit";
+import { CodeBlockLowlight } from "@tiptap/extension-code-block-lowlight";
+import { createLowlight, common } from "lowlight";
+import { Link } from "@tiptap/extension-link";
+import { Table } from "@tiptap/extension-table";
+import { TableRow } from "@tiptap/extension-table-row";
+import { TableCell } from "@tiptap/extension-table-cell";
+import { TableHeader } from "@tiptap/extension-table-header";
+import { TaskList } from "@tiptap/extension-task-list";
+import { TaskItem } from "@tiptap/extension-task-item";
+import { TextAlign } from "@tiptap/extension-text-align";
+import { Highlight } from "@tiptap/extension-highlight";
+import { Superscript } from "@tiptap/extension-superscript";
+import { Subscript } from "@tiptap/extension-subscript";
+import { Placeholder } from "@tiptap/extension-placeholder";
+import { HeadingWithIds } from "#/features/blog/components/tiptap-heading-ids";
+
+const lowlight = createLowlight(common);
+
+export const getEditorExtensions = () => [
+  StarterKit.configure({
+    codeBlock: false,
+    heading: false,
+    link: false,
+  }),
+  HeadingWithIds.configure({
+    levels: [1, 2, 3],
+  }),
+  Link.configure({
+    HTMLAttributes: {
+      rel: "noopener noreferrer",
+      target: "_blank",
+    },
+    openOnClick: false,
+  }),
+  CodeBlockLowlight.configure({
+    defaultLanguage: "plaintext",
+    lowlight,
+  }),
+  Table.configure({
+    resizable: true,
+  }),
+  TableRow,
+  TableCell,
+  TableHeader,
+  TaskList,
+  TaskItem.configure({
+    nested: true,
+  }),
+  TextAlign.configure({
+    types: ["heading", "paragraph"],
+  }),
+  Highlight,
+  Superscript,
+  Subscript,
+  Placeholder.configure({
+    placeholder: "Press / for commands...",
+  }),
+];
